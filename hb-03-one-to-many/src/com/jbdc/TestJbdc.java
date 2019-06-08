33cd58e742c6a3d2e3470c995bab60e824aca1ab
Lucas Oliveira <oliveiraborgeslucas@gmail.com>
2019-06-08 01:12:38 +0100

    new project covering one to many

---
+++ hb-03-one-to-many/src/com/jbdc/TestJbdc.java
@@ -0,0 +1,30 @@
+package com.jbdc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class TestJbdc
+{
+
+	public static void main(String[] args)
+	{
+		String jdbcUrl = "jdbc:mysql://localhost:3306/hb-01-one-to-one-uni?useSSl=false&serverTimezone=UTC";
+		String user = "hbstudent";
+		String pass = "hbstudent";
+
+		try
+		{
+			System.out.println("connecting to database: " + jdbcUrl);
+
+			Connection myConn = DriverManager.getConnection(jdbcUrl, user, pass);
+
+			System.out.println("connetion successful!!!");
+
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+}
