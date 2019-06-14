d1bed5d5c4233b07b27f801fddacd0535abb7c5a
Lucas Oliveira <oliveiraborgeslucas@gmail.com>
2019-06-14 13:51:20 +0100

    Creation of service to improve the architecture

--- web-custumer-tracker/src/com/springdemo/controller/CustomerController.java
+++ web-custumer-tracker/src/com/springdemo/controller/CustomerController.java
@@ -5,26 +5,28 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.springdemo.dao.CustomerDAO;
 import com.springdemo.entity.Customer;
+import com.springdemo.service.CustomerService;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController
 {
 
-	// need to inject the customer dao
+	// need to inject our customer service
 	@Autowired
-	private CustomerDAO customerDAO;
+	private CustomerService customerService;
 
-	@RequestMapping("/list")
+	// @RequestMapping("/list")
+	@GetMapping("/list")
 	public String listCustomers(Model theModel)
 	{
 
-		// get customer from the dao
-		List<Customer> theCustomers = customerDAO.getCustomers();
+		// get customer from the service
+		List<Customer> theCustomers = customerService.getCustomers();
 
 		// add the customer to the model
 		theModel.addAttribute("customers", theCustomers);
