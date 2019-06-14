27f8ea8efbe16371be5b3bdf68fa8116ae19daa1
Lucas Oliveira <oliveiraborgeslucas@gmail.com>
2019-06-14 12:07:49 +0100

    creation of second version of controller, this time showing the real data from db

--- web-custumer-tracker/src/com/springdemo/controller/CustomerController.java
+++ web-custumer-tracker/src/com/springdemo/controller/CustomerController.java
@@ -1,17 +1,34 @@
 package com.springdemo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.springdemo.dao.CustomerDAO;
+import com.springdemo.entity.Customer;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController
 {
 
+	// need to inject the customer dao
+	@Autowired
+	private CustomerDAO customerDAO;
+
 	@RequestMapping("/list")
 	public String listCustomers(Model theModel)
 	{
+
+		// get customer from the dao
+		List<Customer> theCustomers = customerDAO.getCustomers();
+
+		// add the customer to the model
+		theModel.addAttribute("customers", theCustomers);
+
 		return "list-customers";
 	}
 
