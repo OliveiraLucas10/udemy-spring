b3c98798cc63fb954b285938ba021d8533c6bcb1
Lucas Oliveira <oliveiraborgeslucas@gmail.com>
2019-06-23 23:54:07 +0100

    new project to show how to use @AfterReturning

---
+++ spring-demo-aop-after-returning/src/com/aopdemo/aspect/AopExpressions.java
@@ -0,0 +1,33 @@
+package com.aopdemo.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+
+@Aspect
+public class AopExpressions
+{
+
+	@Pointcut("execution(* com.aopdemo.dao.*.*(..))")
+	public void forDaoPackage()
+	{
+	}
+
+	// create a pointcut for getter methods
+	@Pointcut("execution(* com.aopdemo.dao.*.get*(..))")
+	public void getter()
+	{
+	}
+
+	// create a pointcut for setter methods
+	@Pointcut("execution(* com.aopdemo.dao.*.set*(..))")
+	public void setter()
+	{
+	}
+
+	// create a pointcut: include package ... exclude getter/setter
+	@Pointcut(("forDaoPackage() && !(getter() || setter())"))
+	public void forDaoPackageNoGetterSetter()
+	{
+	}
+
+}
