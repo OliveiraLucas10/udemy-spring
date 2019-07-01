fd646b3f8cea48e14a218164c24431d876b78881
Lucas Oliveira <oliveiraborgeslucas@gmail.com>
2019-07-01 10:42:52 +0100

    new project showing on home page users and roles, creation of new pages restricted by role

---
+++ spring-security-demo-07-user-roles/src/main/java/com/oliveira/springsecurity/demo/config/DemoSecurityConfig.java
@@ -0,0 +1,49 @@
+package com.oliveira.springsecurity.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+
+@Configuration
+@EnableWebSecurity
+public class DemoSecurityConfig extends WebSecurityConfigurerAdapter
+{
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception
+	{
+		// add our users for in memory authentication
+
+		UserBuilder users = User.withDefaultPasswordEncoder();
+
+		auth.inMemoryAuthentication().withUser(users.username("jhon").password("test123").roles("EMPLOYEE"));
+		auth.inMemoryAuthentication().withUser(users.username("mary").password("test123").roles("EMPLOYEE", "MANAGER"));
+		auth.inMemoryAuthentication().withUser(users.username("susan").password("test123").roles("EMPLOYEE", "ADMIN"));
+
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception
+	{
+		// Configure security of web paths in application, login, logout etc
+
+		http.authorizeRequests() // restrict access based on the HttpServeletRequest
+						.antMatchers("/").hasRole("EMPLOYEE") // restrict access to a path according with the role (antMatchers("PATH").hasRole("ROLE")
+						.antMatchers("/leaders/**").hasRole("MANAGER") // restrict access to a path according with the role (antMatchers("PATH").hasRole("ROLE") -> /** means any sub path
+						.antMatchers("/systems/**").hasRole("ADMIN") // restrict access to a path according with the role (antMatchers("PATH").hasRole("ROLE") -> /** means any sub path
+						// .anyRequest().authenticated() // any request to the app must be authenticated (ie logged in)
+						.and()
+						.formLogin() // we are customizing the form login process
+						.loginPage("/showMyLoginPage") // show our custom form at the request mapping "/showMyLoginPage"
+						.loginProcessingUrl("/authenticateTheUser") // login form should POST data to this URL for processing (check user id and passowrd)
+						.permitAll() // allow everyone to see login page, no need to be logged in
+						.and()
+						.logout() // add logout support for default URL /logout
+						.permitAll(); // allow everyone to see logout page
+	}
+
+}
---
+++ spring-security-demo-07-user-roles/src/main/webapp/WEB-INF/view/home.jsp
@@ -0,0 +1,62 @@
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<%@ taglib prefix="security" uri="http://www.springframework.org/security/tags" %>
+
+<html>
+
+<head>
+
+<title>Oliveira Company Home Page</title>
+
+
+</head>
+
+<body>
+	<h2>Oliveira Company Home Page</h2>
+
+	<hr>
+
+	<p>
+	Welcome to the Oliveira company home page
+	</p>
+	
+	<hr>
+	
+	<!-- display user name an role -->
+	<p>
+	
+		User: <security:authentication property="principal.username" />
+		<br><br>
+		Role(s): <security:authentication property="principal.authorities" />
+	</p>
+	
+	<hr>
+	
+	<!-- Add a link to point to /leaders ... this is for the managers -->
+	
+	<p>
+		<a href="${pageContext.request.contextPath}/leaders" >LeaderShip Meeting </a>
+		(Only for Manager peeps)
+	</p>
+	
+	<!-- Add a link to point to /systems ... this is for the admins -->
+	
+	<p>
+		<a href="${pageContext.request.contextPath}/systems" >IT Systems Meeting </a>
+		(Only for ADMIN peeps)
+	</p>
+	
+	<hr>
+	
+	<!-- Add a logout button -->
+	<form:form action="${pageContext.request.contextPath}/logout"
+		method="POST">
+
+		<input type="submit" value="Logout" />
+
+	</form:form>
+
+
+</body>
+
+
+</html>
