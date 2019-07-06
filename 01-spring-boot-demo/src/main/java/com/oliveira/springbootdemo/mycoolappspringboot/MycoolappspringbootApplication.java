c6cc992f23e127fb67568d1523a05ce005b10642
Lucas Oliveira <oliveiraborgeslucas@gmail.com>
2019-07-06 14:15:25 +0100

    creation of first project using spring boot

---
+++ 01-spring-boot-demo/src/main/java/com/oliveira/springbootdemo/mycoolappspringboot/MycoolappspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.oliveira.springbootdemo.mycoolappspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycoolappspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MycoolappspringbootApplication.class, args);
+	}
+
+}
