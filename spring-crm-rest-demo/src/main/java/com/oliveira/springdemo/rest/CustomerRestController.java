ef7eb3005db0df09f23cf9367a3d60f1da683065
Lucas Oliveira <oliveiraborgeslucas@gmail.com>
2019-07-06 09:14:22 +0100

    method to update an existing customer

--- spring-crm-rest-demo/src/main/java/com/oliveira/springdemo/rest/CustomerRestController.java
+++ spring-crm-rest-demo/src/main/java/com/oliveira/springdemo/rest/CustomerRestController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -51,6 +52,17 @@ public class CustomerRestController
 		// this is force a save of new item ... instead of update
 		theCustomer.setId(0); // add 0 make DAO insert a new item instead update an existent one
 
+		customerService.saveCustomer(theCustomer);
+
+		return theCustomer;
+	}
+
+	// add mapping for PUT /customers -- update an existing customer
+	@PutMapping("/customers")
+	public Customer updateCustomer(@RequestBody Customer theCustomer)
+	{
+
+		// because id is not null or 0, it means that is an update
 		customerService.saveCustomer(theCustomer);
 
 		return theCustomer;
