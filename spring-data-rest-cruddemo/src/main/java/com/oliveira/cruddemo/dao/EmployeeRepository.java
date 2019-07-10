13ddbf5d6ecef63cb37b37d8cadb5741e02f0266
Lucas Oliveira <oliveiraborgeslucas@gmail.com>
2019-07-10 01:45:49 +0100

    changing the default endpoint in EmployeeRepository and adding new properties to number of elements per page. Here is an example of sorting (http://localhost:8080/magic-api/employees?sort=lastName,desc)

--- spring-data-rest-cruddemo/src/main/java/com/oliveira/cruddemo/dao/EmployeeRepository.java
+++ spring-data-rest-cruddemo/src/main/java/com/oliveira/cruddemo/dao/EmployeeRepository.java
@@ -1,9 +1,12 @@
 package com.oliveira.cruddemo.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.oliveira.cruddemo.entity.Employee;
 
+// changing endpoint instead "employees" now is "members"
+// @RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer>
 {
 
